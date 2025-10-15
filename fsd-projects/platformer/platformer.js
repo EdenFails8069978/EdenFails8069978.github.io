89c7d02a9310db1c9832e5c6ac47854d977cd25c
EdenFails8069978 <erf8069978@lpssonline.com>
2025-10-15 13:25:57 +0000

    todo 4 complete

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -47,10 +47,10 @@ createCollectable("max", 984, 396);
 createCollectable("grace", 100, 200);
 createCollectable("kennedi", 750, 50);
     // TODO 4 - Create Cannons
-createCannon("right", 400, 1000);
-createCannon("bottom", 650, 900);
-createCannon("top", 1000, 800)  
-createCannon("left", 500, 700)
+createCannon("right", 400, 1900);
+createCannon("bottom", 650, 1800);
+createCannon("top", 1000, 1700)  
+createCannon("left", 500, 1600)
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
